57df4d9772e5557c7a1232dfd90ea7371d325873
tgeva <tomerg777@gmail.com>
2022-10-24 10:36:51 +0300

    Changed the LMS test to test with reference pattern. This allows testing for extreme channels where the initial state might not hold the assumption that the slicer errors are scarce

--- test/auxiliary.py
+++ test/auxiliary.py
@@ -1,5 +1,8 @@
 import csv
+import os
 import numpy as np
+import CommDspy as cdsp
+import random
 
 def read_1line_csv(filename, delimiter=','):
     """
@@ -11,3 +14,65 @@ def read_1line_csv(filename, delimiter=','):
         reader = csv.reader(csvfile, delimiter=delimiter)
         ref_prbs_bin = np.array(next(reader)).astype(int)
     return ref_prbs_bin
+
+def generate_pattern(prbs_type):
+    # ==================================================================================================================
+    # Local variables
+    # ==================================================================================================================
+    bits_per_symbol = random.randint(1, 2)
+    constellation = cdsp.constants.ConstellationEnum.PAM4 if bits_per_symbol > 1 else (cdsp.constants.ConstellationEnum.NRZ if random.random() > 0.5 else cdsp.constants.ConstellationEnum.OOK)
+    gray_coding = False  # random.random() > 0.5
+    poly_coeff = cdsp.get_polynomial(prbs_type)
+    init_seed = np.array([1] * prbs_type.value)
+    prbs_len = 2 ** len(init_seed) - 1
+    assert poly_coeff.any() is not None, prbs_type.name + ' type not supported'
+    ref_filename = os.path.join(os.getcwd(), 'test_data', prbs_type.name + '_seed_ones.csv')
+    # ==================================================================================================================
+    # Creating reference pattern
+    # ==================================================================================================================
+    # --------------------------------------------------------------------------------------------------------------
+    # Loading pattern
+    # --------------------------------------------------------------------------------------------------------------
+    ref_prbs_bin = read_1line_csv(ref_filename)
+    # --------------------------------------------------------------------------------------------------------------
+    # Duplicating if needed and coding
+    # --------------------------------------------------------------------------------------------------------------
+    if bits_per_symbol > 1:
+        ref_prbs_bin_mult = np.tile(ref_prbs_bin, bits_per_symbol)
+        ref_pattern = cdsp.tx.bin2symbol(ref_prbs_bin_mult, 2 ** bits_per_symbol, False, False, False, False)
+    else:
+        ref_pattern = ref_prbs_bin
+    ref_pattern = cdsp.tx.mapping(ref_pattern, constellation) if not gray_coding else cdsp.tx.mapping(cdsp.tx.coding_gray(ref_pattern, constellation), constellation)
+    # --------------------------------------------------------------------------------------------------------------
+    # Creating repetitions of the pattern
+    # --------------------------------------------------------------------------------------------------------------
+    reps = random.randint(2, 5)
+    cutoff = random.randint(1, int(prbs_len / 2))
+    ref_pattern = np.tile(ref_pattern, reps)[:-1 * cutoff]
+
+    return ref_pattern, constellation, gray_coding
+
+def generate_and_pass_channel(ref_pattern, prbs_type, constellation, gray_coding):
+    num_poles = random.randint(1, 8)
+    assert_str = '|{0:^6s}| Constellation = {1:^6s} | Gray Coding = {2:^6s} | {3:^3d} poles  ; {4:^3d} DFE taps '.format(
+        prbs_type.name,
+        constellation.name,
+        str(gray_coding),
+        num_poles,
+        0
+    )
+    # ==================================================================================================================
+    # Creating reference channel
+    # ==================================================================================================================
+    while True:
+        roots       = np.around(np.random.random(num_poles), decimals=3) - 0.5
+        channel_ref = np.poly(roots)
+        if np.max(np.abs(channel_ref)) == channel_ref[0]:
+            break
+    # ==================================================================================================================
+    # Passing data through the channel
+    # ==================================================================================================================
+    channel_out, _ = cdsp.channel.awgn_channel(ref_pattern, [1], channel_ref)
+    channel_out    = channel_out[len(channel_ref) + 1:]
+
+    return channel_ref, channel_out, constellation, assert_str
--- test/test_equalization_estimation.py
+++ test/test_equalization_estimation.py
@@ -3,7 +3,7 @@ from scipy import signal
 import os
 import CommDspy as cdsp
 import random
-from test.auxiliary import read_1line_csv
+from test.auxiliary import generate_pattern, generate_and_pass_channel
 from CommDspy.rx.ffe_dfe_model import ffe_dfe
 
 
@@ -14,7 +14,7 @@ def equalization_prbs_test(prbs_type):
              is a FIR and can be thus be checked according to the reference IIR. Nevertheless, the IIR should have all
              poles inside the unit circle, or else the resulting signal will be unstable
     """
-    channel_ref, channel_out, constellation, assert_str =  _get_test_parameters(prbs_type)
+    channel_ref, channel_out, constellation, _, assert_str =  _get_test_parameters(prbs_type)
     # ==================================================================================================================
     # Running DUT
     # ==================================================================================================================
@@ -45,7 +45,7 @@ def equalization_prbs_test(prbs_type):
 
 
 def equalization_lms_test(prbs_type):
-    channel_ref, channel_out, constellation, assert_str =  _get_test_parameters(prbs_type)
+    channel_ref, channel_out, constellation, ref_pattern, assert_str =  _get_test_parameters(prbs_type)
     precursors        = np.argmax(abs(channel_ref))
     postcursors       = len(channel_ref) - precursors - 1
     pn_inv_postcoding = channel_ref[precursors] != np.max(channel_ref)
@@ -55,16 +55,25 @@ def equalization_lms_test(prbs_type):
     # ==================================================================================================================
     equ_dut             = np.zeros_like(channel_ref)
     equ_dut[precursors] = 1
-    mse_vec = []
+    mse_vec = [1e3]
     count   = 0
-    while count < 10000:
+    while count < 100000 and mse_vec[-1] > 1e-5:
         count += 1
+        # ----------------------------------------------------------------------------------------------------------
+        # Passing through the FFE + DFE
+        # ----------------------------------------------------------------------------------------------------------
         slicer_in_dut = ffe_dfe(channel_out, equ_dut)
-        mse, grad_ffe = cdsp.rx.lms_grad(slicer_in_dut, cdsp.get_levels(constellation), tap_idx_vec)
-        mse_vec.append(mse)
-        equ_dut -= grad_ffe * 0.001
-        if abs(mse - mse_vec[-1]) < 1e-5:
+        # ----------------------------------------------------------------------------------------------------------
+        # Aligning
+        # ----------------------------------------------------------------------------------------------------------
+        prbs_len = 2 ** prbs_type.value - 1
+        pattern_aligned, _  = cdsp.rx.lock_pattern_to_signal(ref_pattern[:prbs_len], slicer_in_dut)
+        pattern_aligned_rep = np.tile(pattern_aligned, int(np.ceil(len(ref_pattern) / prbs_len)))[:len(slicer_in_dut)]
+        mse, grad_ffe = cdsp.rx.lms_grad(slicer_in_dut, cdsp.get_levels(constellation), tap_idx_vec, reference_vec=pattern_aligned_rep)
+        equ_dut -= grad_ffe * 0.003
+        if abs(mse - mse_vec[-1]) < 1e-10:
             break
+        mse_vec.append(mse)
     # ==================================================================================================================
     # Comparing results
     # ==================================================================================================================
@@ -72,60 +81,6 @@ def equalization_lms_test(prbs_type):
 
 
 def _get_test_parameters(prbs_type):
-    # ==================================================================================================================
-    # Local variables
-    # ==================================================================================================================
-    bits_per_symbol = random.randint(1, 2)
-    num_poles       = random.randint(1, 8)
-    constellation   = cdsp.constants.ConstellationEnum.PAM4 if bits_per_symbol > 1 else (cdsp.constants.ConstellationEnum.NRZ if random.random() > 0.5 else cdsp.constants.ConstellationEnum.OOK)
-    gray_coding     = False  # random.random() > 0.5
-    poly_coeff      = cdsp.get_polynomial(prbs_type)
-    init_seed       = np.array([1] * prbs_type.value)
-    prbs_len        = 2 ** len(init_seed) - 1
-    assert poly_coeff.any() is not None, prbs_type.name + ' type not supported'
-    ref_filename = os.path.join(os.getcwd(), 'test_data', prbs_type.name + '_seed_ones.csv')
-    assert_str = '|{0:^6s}| Constellation = {1:^6s} | Gray Coding = {2:^6s} | {3:^3d} poles  ; {4:^3d} DFE taps '.format(
-        prbs_type.name,
-        constellation.name,
-        str(gray_coding),
-        num_poles,
-        0
-    )
-    # ==================================================================================================================
-    # Creating reference pattern
-    # ==================================================================================================================
-    # --------------------------------------------------------------------------------------------------------------
-    # Loading pattern
-    # --------------------------------------------------------------------------------------------------------------
-    ref_prbs_bin = read_1line_csv(ref_filename)
-    # --------------------------------------------------------------------------------------------------------------
-    # Duplicating if needed and coding
-    # --------------------------------------------------------------------------------------------------------------
-    if bits_per_symbol > 1:
-        ref_prbs_bin_mult = np.tile(ref_prbs_bin, bits_per_symbol)
-        ref_pattern = cdsp.tx.bin2symbol(ref_prbs_bin_mult, 2 ** bits_per_symbol, False, False, False, False)
-    else:
-        ref_pattern = ref_prbs_bin
-    ref_pattern = cdsp.tx.mapping(ref_pattern, constellation) if not gray_coding else cdsp.tx.mapping(
-        cdsp.tx.coding_gray(ref_pattern, constellation), constellation)
-    # --------------------------------------------------------------------------------------------------------------
-    # Creating repetitions of the pattern
-    # --------------------------------------------------------------------------------------------------------------
-    reps = random.randint(2, 5)
-    cutoff = random.randint(1, int(prbs_len / 2))
-    ref_pattern = np.tile(ref_pattern, reps)[:-1 * cutoff]
-    # ==================================================================================================================
-    # Creating reference channel
-    # ==================================================================================================================
-    while True:
-        roots = np.around(np.random.random(num_poles), decimals=3) - 0.5
-        channel_ref = np.poly(roots)
-        if np.max(np.abs(channel_ref)) == channel_ref[0]:
-            break
-    # ==================================================================================================================
-    # Passing data through the channel
-    # ==================================================================================================================
-    channel_out, _ = cdsp.channel.awgn_channel(ref_pattern, [1], channel_ref)
-    channel_out = channel_out[len(channel_ref) + 1:]
-
-    return channel_ref, channel_out, constellation, assert_str
+    ref_pattern, constellation, gray_coding = generate_pattern(prbs_type)
+    channel_ref, channel_out, constellation, assert_str = generate_and_pass_channel(ref_pattern, prbs_type, constellation, gray_coding)
+    return channel_ref, channel_out, constellation, ref_pattern, assert_str
